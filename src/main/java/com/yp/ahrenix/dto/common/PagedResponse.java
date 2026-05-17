6e501dcab902bb7f5517039ec8a66e174c92a760
Aditya_2K03 <yogeeswarp@gmail.com>
2026-05-17 18:02:18 +0530

    added dto layer, added the common dto's and initiated the request and response dto's

---
+++ src/main/java/com/yp/ahrenix/dto/common/PagedResponse.java
@@ -0,0 +1,26 @@
+package com.yp.ahrenix.dto.common;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagedResponse<T> {
+
+    private List<T> content;
+
+    private int page;
+
+    private int size;
+
+    private long totalElements;
+
+    private int totalPages;
+
+    private boolean last;
+
+}
