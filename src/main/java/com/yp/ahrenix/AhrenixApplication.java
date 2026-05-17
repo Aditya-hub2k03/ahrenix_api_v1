6df4346003fec2b5530fc5c8871e1b7a48edf69c
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2026-05-17 17:08:13 +0530

    Add files via upload

---
+++ src/main/java/com/yp/ahrenix/AhrenixApplication.java
@@ -0,0 +1,13 @@
+package com.yp.ahrenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhrenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhrenixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yp/ahrenix/AhrenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yp.ahrenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhrenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
