daad701252dbd99e2fa303c3e7fac2aa01019cec
Aditya_2K03 <yogeeswarp@gmail.com>
2026-05-19 14:51:00 +0530

    bugs fixes done in mapper layer

--- src/main/java/com/yp/ahrenix/mapper/TransactionMapper.java
+++ src/main/java/com/yp/ahrenix/mapper/TransactionMapper.java
@@ -1,6 +1,7 @@
 package com.yp.ahrenix.mapper;
 
 import com.yp.ahrenix.dto.response.TransactionResponse;
+import com.yp.ahrenix.entities.Account;
 import com.yp.ahrenix.entities.Transaction;
 import org.mapstruct.Mapper;
 
@@ -8,5 +9,9 @@ import org.mapstruct.Mapper;
 public interface TransactionMapper {
 
     TransactionResponse toResponse(Transaction transaction);
+
+    default String map(Account account) {
+        return account == null ? null : account.toString();
+    }
 
 }
--- src/main/java/com/yp/ahrenix/mapper/UserMapper.java
+++ src/main/java/com/yp/ahrenix/mapper/UserMapper.java
@@ -1,12 +1,19 @@
 package com.yp.ahrenix.mapper;
 
 import com.yp.ahrenix.dto.response.UserResponse;
+import com.yp.ahrenix.entities.Role;
 import com.yp.ahrenix.entities.User;
+import com.yp.ahrenix.enums.RoleType;
+
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserResponse toResponse(User user);
+
+    default RoleType map(Role role) {
+        return role == null ? null : role.getName();
+    }
 
 }
