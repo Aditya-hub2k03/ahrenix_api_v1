e0c4c407cc99595a67db569dbde0d9e5c5d5cafa
Aditya_2K03 <yogeeswarp@gmail.com>
2026-05-22 18:54:14 +0530

    updated the user mapping from the usermapper

--- src/main/java/com/yp/ahrenix/mapper/UserMapper.java
+++ src/main/java/com/yp/ahrenix/mapper/UserMapper.java
@@ -3,17 +3,23 @@ package com.yp.ahrenix.mapper;
 import com.yp.ahrenix.dto.response.UserResponse;
 import com.yp.ahrenix.entities.Role;
 import com.yp.ahrenix.entities.User;
-import com.yp.ahrenix.enums.RoleType;
+import org.mapstruct.*;
 
-import org.mapstruct.Mapper;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "roles",
+            expression = "java(mapRoles(user.getRoles()))")
     UserResponse toResponse(User user);
 
-    default RoleType map(Role role) {
-        return role == null ? null : role.getName();
+    default Set<String> mapRoles(Set<Role> roles) {
+
+        return roles.stream()
+                .map(role -> role.getName().name())
+                .collect(Collectors.toSet());
     }
 
 }
