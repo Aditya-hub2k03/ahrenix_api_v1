aedfe6f8f61c7ee2f1b73969e09d05ce2f453bff
Aditya_2K03 <yogeeswarp@gmail.com>
2026-05-22 18:11:12 +0530

    updated the blank service in the transaction service class

--- src/main/java/com/yp/ahrenix/service/TransactionService.java
+++ src/main/java/com/yp/ahrenix/service/TransactionService.java
@@ -2,11 +2,19 @@ package com.yp.ahrenix.service;
 
 import com.yp.ahrenix.dto.request.TransactionRequest;
 import com.yp.ahrenix.dto.response.TransactionResponse;
+import com.yp.ahrenix.entities.Account;
 import com.yp.ahrenix.entities.Transaction;
 import com.yp.ahrenix.entities.User;
+import com.yp.ahrenix.enums.TransactionStatus;
+import com.yp.ahrenix.enums.TransactionType;
+import com.yp.ahrenix.exception.InsufficientBalanceException;
 import com.yp.ahrenix.exception.ResourceNotFoundException;
+import com.yp.ahrenix.mapper.TransactionMapper;
+import com.yp.ahrenix.repository.AccountRepository;
 import com.yp.ahrenix.repository.TransactionRepository;
+import com.yp.ahrenix.util.TransactionReferenceGenerator;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +24,10 @@ import org.springframework.stereotype.Service;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+    private final TransactionMapper transactionMapper;
+    private final KafkaProducerService kafkaProducerService;
+    private final AuditService auditService;
 
     public Transaction getTransactionByReferenceId(
             String referenceId
@@ -29,9 +41,86 @@ public class TransactionService {
                 );
     }
 
-    public TransactionResponse transferMoney(User user, TransactionRequest request, String remoteAddr) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'transferMoney'");
+    @Transactional
+public TransactionResponse transferMoney(
+        User user,
+        TransactionRequest request,
+        String ipAddress
+) {
+
+    Account senderAccount =
+            accountRepository.findByUser(user)
+                    .stream()
+                    .findFirst()
+                    .orElseThrow(() ->
+                            new ResourceNotFoundException(
+                                    "Sender account not found"
+                            )
+                    );
+
+    Account receiverAccount =
+            accountRepository.findByAccountNumber(
+                    request.getReceiverAccountNumber()
+            )
+                    .orElseThrow(() ->
+                            new ResourceNotFoundException(
+                                    "Receiver account not found"
+                            )
+                    );
+
+    if (senderAccount.getBalance()
+            .compareTo(request.getAmount()) < 0) {
+
+        throw new InsufficientBalanceException(
+                "Insufficient balance"
+        );
     }
+
+    senderAccount.setBalance(
+            senderAccount.getBalance()
+                    .subtract(request.getAmount())
+    );
+
+    receiverAccount.setBalance(
+            receiverAccount.getBalance()
+                    .add(request.getAmount())
+    );
+
+    accountRepository.save(senderAccount);
+    accountRepository.save(receiverAccount);
+
+    Transaction transaction = Transaction.builder()
+            .referenceId(
+                    TransactionReferenceGenerator
+                            .generateReference()
+            )
+            .transactionType(TransactionType.TRANSFER)
+            .transactionStatus(TransactionStatus.SUCCESS)
+            .amount(request.getAmount())
+            .description(request.getDescription())
+            .senderAccount(senderAccount)
+            .receiverAccount(receiverAccount)
+            .build();
+
+    Transaction savedTransaction =
+            transactionRepository.save(transaction);
+
+    kafkaProducerService.sendMessage(
+            "transactions-created",
+            "Transaction Created: " +
+                    savedTransaction.getReferenceId()
+    );
+
+    auditService.logAction(
+            "TRANSFER_MONEY",
+            "TRANSACTION",
+            user.getEmail(),
+            "Transferred " +
+                    request.getAmount(),
+            ipAddress
+    );
+
+    return transactionMapper.toResponse(savedTransaction);
+}
 
 }
