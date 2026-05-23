c22ecda666f6a951a9b50c576d40b0b95b379ec2
Aditya_2K03 <yogeeswarp@gmail.com>
2026-05-23 11:05:07 +0530

    updated transaction mapper with the transaction respone dto additions also

--- src/main/java/com/yp/ahrenix/dto/response/TransactionResponse.java
+++ src/main/java/com/yp/ahrenix/dto/response/TransactionResponse.java
@@ -32,4 +32,8 @@ public class TransactionResponse {
 
     private LocalDateTime createdAt;
 
+    private String senderName;
+    
+    private String receiverName;
+
 }
--- src/main/java/com/yp/ahrenix/mapper/TransactionMapper.java
+++ src/main/java/com/yp/ahrenix/mapper/TransactionMapper.java
@@ -1,17 +1,31 @@
 package com.yp.ahrenix.mapper;
 
 import com.yp.ahrenix.dto.response.TransactionResponse;
-import com.yp.ahrenix.entities.Account;
 import com.yp.ahrenix.entities.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
 
-    TransactionResponse toResponse(Transaction transaction);
-
-    default String map(Account account) {
-        return account == null ? null : account.toString();
-    }
+    @Mapping(
+            source = "senderAccount.accountNumber",
+            target = "senderAccount"
+    )
+    @Mapping(
+            source = "receiverAccount.accountNumber",
+            target = "receiverAccount"
+    )
+    @Mapping(
+            source = "senderAccount.user.fullName",
+            target = "senderName"
+    )
+    @Mapping(
+            source = "receiverAccount.user.fullName",
+            target = "receiverName"
+    )
+    TransactionResponse toResponse(
+            Transaction transaction
+    );
 
 }
